90dcf783eee0e5b02de3e239f1ed37fbde26703d
Abner <2906631584@qq.com>
2018-06-14 00:51:33 +0800

    在Object中添加测试数据

---
+++ Object/test.c
@@ -0,0 +1,11 @@
+//
+// Created by Abner on 2018/6/14.
+//
+#include "Python.h"
+
+static int f(int a, int b) {
+    int c = a + b;
+    printf("c is %d\n", c);
+    return c;
+}
+
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "Python.h"
-
+#include "test.c"
 int main() {
     printf("Hello, World!\n");
+    f(5, 5);
+
     return 0;
 }
